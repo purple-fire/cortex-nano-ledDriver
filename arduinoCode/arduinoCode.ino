3015a7216394d002a393b6b7299e10b3baa0f59a
zdog1230 <zdog1230@yahoo.com>
2018-04-19 15:39:11 -0400

    Add option to change teams

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -2,12 +2,12 @@
 #include <SoftwareSerial.h>
 
 //Use Software Serial to Read from Cortex
-SoftwareSerial mySerial(11, 12); // RX, TX
+SoftwareSerial mySerial(10, 11); // RX, TX
 
-// How many leds are in the strip?
-#define NUM_LEDS 26
+// How many leds per strip
+#define NUM_LEDS 23
 
-// Data pin that led data will be written out over
+// Data pins for led strips
 #define DATA1_PIN 3
 #define DATA2_PIN 6
 
@@ -17,7 +17,11 @@ CRGB leds[NUM_LEDS];
 //Store Current Count
 int count = 0;
 
-int incomingByte = 0;   // for incoming serial data
+//Store Team Value (0 BLUE,1 RED)
+int team = 0;
+
+// for incoming serial data
+int incomingByte = 0;
 
 // This function sets up the leds and tells the controller about them
 void setup() {
@@ -37,6 +41,7 @@ void setup() {
       
       FastLED.addLeds<WS2812B, DATA1_PIN, RGB>(leds, NUM_LEDS);
       FastLED.addLeds<WS2812B, DATA2_PIN, RGB>(leds, NUM_LEDS);
+      FastLED.setBrightness(100); 
 }
 
 // This function runs over and over, and is where you do the magic to light
@@ -44,13 +49,19 @@ void setup() {
 void updateLEDs() {
    for(int i = 0; i < NUM_LEDS; i = i + 1) {
     if(i<count){
-      leds[i].setRGB( 255, 255, 0);
+      //Set Color Based on Team
+      if(team==0){
+        leds[i].setRGB( 0, 0, 100);
+      }
+      else if(team==1){
+        leds[i].setRGB( 0, 100, 0);
+      }
+      //Otherwise Off
     } else {
       leds[i].setRGB( 0, 0, 0);
     }
    }
-   delay(100);
-   FastLED.show();
+
 }
 
 void loop(){
@@ -61,11 +72,26 @@ void loop(){
                 //Serial.print("I received: ");
                 //Serial.println(incomingByte-97, DEC);
 
-                //Set Count to What is Recieved
-                count = incomingByte-97;
+                //Switch Based of Letter Receieved
+                switch(incomingByte) {
+                  //Change to Blue Team
+                  case 'y':
+                    team =0;
+                    break;
+                  //Change to Red Team
+                  case 'z':
+                    team = 1;
+                    break;
+                  //Set Count to What is Recieved
+                  default:
+                    count = incomingByte-'a';
+                    updateLEDs();
+                }
+
+               
         }
-        //Update LEDs
-        updateLEDs();
+        delay(100);
+        FastLED.show();
 }
 
 
